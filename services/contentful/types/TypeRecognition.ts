09271382eb024cc926463215e36a6b843783ea5f
Max Thorson <max@mst.mn>
2023-12-13 11:15:29 -0700

    new types

--- services/contentful/types/TypeRecognition.ts
+++ services/contentful/types/TypeRecognition.ts
@@ -1,9 +1,11 @@
 import * as Contentful from "contentful";
+import { TypeProjectFields } from "./TypeProject";
 
 export interface TypeRecognitionFields {
     name: Contentful.EntryFields.Symbol;
     type: "award" | "exhibition" | "publication";
     image: Contentful.Asset;
+    project?: Contentful.Entry<TypeProjectFields>;
     projectPageImage?: Contentful.Asset;
     body: Contentful.EntryFields.Text;
     year: Contentful.EntryFields.Integer;
